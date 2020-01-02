6f287610ad399c48a2b80e10a05fa576147feaed
林木木 <fivemu@gmail.com>
2020-01-02 23:36:14 +0800

    v.0.2.0

--- breek/assets/media/js/functions.js
+++ breek/assets/media/js/functions.js
@@ -12,7 +12,7 @@
         
         
 		if($(document).width() > 767){
-            var $grid = $('.enable-masonry .grid-posts div.articles:not(.classic)').masonry({
+            var $grid = $('.enable-masonry .grid-posts div.articles:not(.fullcover)').masonry({
                 itemSelector: 'article',
                 gutter: 0,
                 horizontalOrder: true,
@@ -34,8 +34,18 @@
 
 	$(document).ready(function($){
 
-        $.lately({
-            'target' : '.lately'
+        $.viewImage({
+            'target' : '.post-content img', //需要使用ViewImage的图片
+            'exclude': '',    //要排除的图片
+            'delay'  : 300                //延迟时间
+        });
+
+        var parse_url = /^(?:([A-Za-z]+):)?(\/{0,3})([0-9.\-A-Za-z]+)(?::(\d+))?(?:\/([^?#]*))?(?:\?([^#]*))?(?:#(.*))?$/;
+        var location_href = window.location.href.replace(parse_url,'$3');
+        $('.post-excerpt a:not(:has(img)),.post-content a:not(:has(img)),.author-name a').hover(function() {
+            var this_href = $(this).attr('href');
+            var replace_href = this_href.replace(parse_url,'$3');
+            if ( this_href != replace_href && location_href != replace_href){$(this).attr('target','_blank');}
         });
 
         var rtl = false;
@@ -60,29 +70,12 @@
                 e.preventDefault();
             });
 		}
-        
-
-		// Counter animation
-
-		$('.count').each(function () {
-			$(this).prop('Counter',0).animate({
-				Counter: $(this).data('total')
-			}, {
-				duration: 1500,
-				easing: 'swing',
-				step: function (now) {
-					$(this).text(Math.ceil(now));
-				}
-			});
-        });
 
 
         // Custom Select and filters
-
 		$('#header div.menu-mobile').on('click', function(){
 			$('#header').toggleClass('menu-open');
 		});
-
 		$('.filters select').change(function(){
 			var url = $(this).val();
 			if(url)
@@ -90,151 +83,24 @@
         });
 
         // Back to top button
-
-		$('#back-to-top').on('click', function(event) {
-			event.preventDefault();
-			$('html, body').animate({scrollTop: 0}, 500);
-			return false;
-        });
-
-        // Single Post copy button
-
-        $(".permalink .copy").on('click', function(){
-            $("#copy-link").select();
-            document.execCommand('copy');
-        });
-
-		// Gallery Post Format
-
-        $('.post-format-gallery .slick-slider').each(function(){
-            var rtl = false;
-            if( parseInt( $(this).data('rtl') ) > 0 ){
-                rtl = true;
+        var bt;
+        bt = $('#back-to-top');
+        if ($(document).width() > 480) {
+          $(window).scroll(function() {
+            var st;
+            st=$(window).scrollTop();
+            if (st > 500) {
+              return bt.css('display', 'block');
+            } else {
+              return bt.css('display', 'none');
             }
-            $(this).slick({
-                cssEase: 'ease',
-                fade: true,
-                arrows: true,
-                infinite: true,
-                dots: false,
-                autoplay: false,
-                speed: 600,
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                rtl: rtl,
-            });
-        });
-
-
-        // Module: carousel
-
-		$('.epcl-carousel').each(function(index, el) {
-            var slides_to_show = parseInt( $(this).data('show') );
-            var rtl = false;
-            if( parseInt( $(this).data('rtl') ) > 0 ){
-                rtl = true;
-            }
-			$(this).slick({
-				cssEase: 'ease',
-				fade: false,
-				arrows: true,
-				infinite: true,
-				dots: false,
-				autoplay: false,
-                speed: 600,
-                rtl: rtl,
-				slidesToShow: slides_to_show,
-				slidesToScroll: slides_to_show,
-                responsive: [,
-                    {
-                        breakpoint: 1700,
-                        settings: {
-                            slidesToShow: 4,
-                            slidesToScroll: 4
-                        }
-                    },
-                    {
-                        breakpoint: 1200,
-                        settings: {
-                            slidesToShow: 3,
-                            slidesToScroll: 3
-                        }
-                    },
-                    {
-                        breakpoint: 980,
-                        settings: {
-                            slidesToShow: 2,
-                            slidesToScroll: 2
-                        }
-                    },
-                    {
-                        breakpoint: 767,
-                        settings: {
-                            slidesToShow: 1,
-                            slidesToScroll: 1
-                        }
-                    },
-                ]
-			});
-		});
-
-		// Global: lightbox
-
-		$('.lightbox').magnificPopup({
-			mainClass: 'my-mfp-zoom-in',
-			removalDelay: 300,
-			closeMarkup: '<i title="%title%" class="mfp-close fa fa-times"></i>',
-			fixedContentPos: true
-        });
-
-        // Global: related galleries
-
-        $('.epcl-gallery').each(function() {
-            var elem = $(this);
-            elem.find('ul').magnificPopup({
-                type: 'image',
-                gallery:{
-                    enabled: true,
-                    arrowMarkup: '<i class="mfp-arrow mfp-arrow-%dir% fa fa-chevron-%dir%"></i>',
-                    tCounter: '%curr% / %total%'
-                },
-                delegate: 'a',
-                mainClass: 'my-mfp-zoom-in',
-                removalDelay: 300,
-                closeMarkup: '<span title="%title%" class="mfp-close">&times;</span>'
-            });
-        });
-
-        // Gutenberg Gallery with lightbox
-        $('.post-content,.post-excerpt').each(function() {
-            var elem = $(this);
-            elem.magnificPopup({
-                type: 'image',
-                gallery:{
-                    enabled: true,
-                    arrowMarkup: '<i class="mfp-arrow mfp-arrow-%dir% fa fa-chevron-%dir%"></i>',
-                    tCounter: '%curr% / %total%'
-                },
-                delegate: "a[href$='.jpg'],a[href$='.jpeg'],a[href$='.png'],a[href$='.gif']",
-                mainClass: 'my-mfp-zoom-in',
-                removalDelay: 300,
-                closeMarkup: '<span title="%title%" class="mfp-close">&times;</span>'
-            });
-        });
-
-        // Gutenberg Single Image with lightbox
-        $('.wp-block-image').magnificPopup({
-            type: 'image',
-            gallery:{
-                enabled: true,
-                arrowMarkup: '<i class="mfp-arrow mfp-arrow-%dir% fa fa-chevron-%dir%"></i>',
-                tCounter: '%curr% / %total%'
-            },
-            delegate: "a[href$='.jpg'],a[href$='.jpeg'],a[href$='.png'],a[href$='.gif']",
-            mainClass: 'my-mfp-zoom-in',
-            removalDelay: 300,
-            closeMarkup: '<span title="%title%" class="mfp-close">&times;</span>'
-        });
+          });
+          return bt.on('click', function(event) {
+            event.preventDefault();
+            $('html, body').animate({scrollTop: 0}, 500);
+            return false;
+          });
+        }
 
 	});
 
